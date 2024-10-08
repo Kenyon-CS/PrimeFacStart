b175e6807aa788affb22dda19300d8f553dea48f
James Skön <skonjp@kenyon.edu>
2024-10-08 12:13:11 -0400

    Create primeFac.cpp

---
+++ primeFac.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+nt main() {
+    int number;
+    cout << "Enter a number: ";
+    cin >> number;
+    
+    if (number < 2) {
+        cout << "No prime factors for numbers less than 2." << endl;
+        return 0;
+    }
+
+    // compute and display prime factors of number
+
+    return 0;
+}
